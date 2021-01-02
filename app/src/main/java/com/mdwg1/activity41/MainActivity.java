083ca9a598f5fbc962970d6fab5a4226e2ba5ef2
youssefbenjaber <youssefbenjaber@yahoo.com>
2021-01-02 11:34:44 +0100

    Initial commit

---
+++ app/src/main/java/com/mdwg1/activity41/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mdwg1.activity41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
